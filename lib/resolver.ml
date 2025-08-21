d98f19e07694103a5977d0808fac3ffc1e3ff1d9
gr-im <grimfw@gmail.com>
2025-08-21 11:10:30 +0200

    Introduce resolver
    Resolvers are clearly useful when dealing with complex paths, but I
    wasn't very keen on the object-based approaches found in Kane[1] and
    the module-based approaches found in Capsule[2]. I think it's enough
    to have a very simple type and functions that serve the resolutions.
    
    [1] https://github.com/xvw/kane
    [2] https://github.com/xvw/capsule
    
    Co-authored-by: Xavier Van de Woestyne <xaviervdw@gmail.com>

---
+++ lib/resolver.ml
@@ -0,0 +1,69 @@
+module Path = Yocaml.Path
+
+type t =
+  { cache : Path.t
+  ; source : Path.t
+  ; target : Path.t
+  ; server_root : Path.t
+  }
+
+let make
+      ?(cache_folder = Path.rel [ "_cache" ])
+      ?(source_folder = Path.rel [])
+      ?(target_folder = Path.rel [ "_www" ])
+      ?(server_root = Path.abs [])
+      ()
+  =
+  { cache = cache_folder
+  ; source = source_folder
+  ; target = target_folder
+  ; server_root
+  }
+;;
+
+let trim ~prefix path =
+  let kind_a, a = Path.to_pair prefix
+  and kind_b, b = Path.to_pair path in
+  let rec aux x y =
+    match x, y with
+    | [], ys -> ys
+    | _, [] -> b
+    | prefix_a :: xs, prefix_b :: ys when String.equal prefix_a prefix_b ->
+      aux xs ys
+    | _ -> b
+  in
+  match kind_a, kind_b with
+  | `Rel, `Rel -> Path.rel (aux a b)
+  | `Root, `Root -> Path.abs (aux a b)
+  | `Root, `Rel | `Rel, `Root ->
+    (* No common prefix *)
+    path
+;;
+
+module Source = struct
+  let source { source; _ } = source
+  let assets r = Path.(source r / "assets")
+  let css r = Path.(assets r / "css")
+end
+
+module Target = struct
+  let target { target; server_root; _ } = Path.relocate ~into:target server_root
+  let assets r = Path.(target r / "assets")
+  let css r = Path.(assets r / "css")
+end
+
+module Cache = struct
+  let cache { cache; _ } = cache
+  let global r = Path.(cache r / "yocaml")
+end
+
+module Server = struct
+  let server { server_root; _ } = server_root
+
+  let from_target r p =
+    p |> trim ~prefix:(Target.target r) |> Path.relocate ~into:(server r)
+  ;;
+
+  let assets r = from_target r (Target.assets r)
+  let css r = from_target r (Target.css r)
+end
---
+++ lib/resolver.mli
@@ -0,0 +1,85 @@
+(** The Resolver allows you to resolve different paths (whether they
+    are paths from the source, target, etc.).
+
+    It is generally transmitted almost everywhere and functions as a
+    module whose configuration is defined when the generator is
+    executed. *)
+
+(** {1 Types} *)
+
+(** The resolver, passed from function to function. It contains all
+    the data needed to resolve any target and locate any source. *)
+type t
+
+(** Initialise a resolver.
+    - [cache_folder] describes the directory
+      where the cache will be stored, by default: [./_cache]
+    - [source_folder] describes the directory
+      where source are stored, by default: [./]
+    - [target_folder] describes the folder where the site will
+      be generated, by default: [./_www]
+    - [server_root] describe the subfolder where the website
+      is served, by default: [/]
+
+    The real target is computed by [target_folder / server_root] in order
+    to manage generation into subdirectories. *)
+val make
+  :  ?cache_folder:Yocaml.Path.t
+  -> ?source_folder:Yocaml.Path.t
+  -> ?target_folder:Yocaml.Path.t
+  -> ?server_root:Yocaml.Path.t
+  -> unit
+  -> t
+
+(** {1 Source}
+
+    Resolver dedicated to resolving source element (raw content). *)
+
+module Source : sig
+  (** All sources elements. *)
+
+  val assets : t -> Yocaml.Path.t
+  val css : t -> Yocaml.Path.t
+end
+
+(** {1 Cache}
+
+    Resolver dedicated to resolving cache elements *)
+
+module Cache : sig
+  (** The cache stores various elements that are useful for generating
+      pages and intermediate states. *)
+
+  (** Returns the path where the cache dedicated to YOCaml is located. *)
+  val global : t -> Yocaml.Path.t
+end
+
+(** {1 Target}
+
+    Resolver dedicated to resolving target. *)
+
+module Target : sig
+  (** All targets describe paths in which files are potentially
+      created. *)
+
+  val assets : t -> Yocaml.Path.t
+  val css : t -> Yocaml.Path.t
+end
+
+(** {1 Server}
+
+    Resolver dedicated to resolving target from the root of the
+    server. *)
+
+module Server : sig
+  (** Resolve path from a server perspective. i.e: [_www/home/index]
+      is [/www/home/index]. *)
+
+  (** Converts a path from the target as if it were served by the
+      server. For example: [_www/foo/index.html] becomes
+      [/foo/index.html]. *)
+  val from_target : t -> Yocaml.Path.t -> Yocaml.Path.t
+
+  val assets : t -> Yocaml.Path.t
+  val css : t -> Yocaml.Path.t
+end
