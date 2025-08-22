f85eb81eed704055ab9e1a9bae04d3c5f236990e
gr-im <grimfw@gmail.com>
2025-08-22 16:36:33 +0200

    As for Capsule [1], we relay on a global config
    Although we regret the somewhat aggressive sealing of YOCaml's
    effects, we assume that each page will have to be built on the basis
    of a general configuration.
    
    [2] https://github.com/xvw/capsule
    
    Co-authored-by: Xavier Van de Woestyne <xaviervdw@gmail.com>

---
+++ lib/model/configuration.ml
@@ -0,0 +1,55 @@
+type t =
+  { title : string
+  ; subtitle : string
+  ; main_url : Url.t
+  ; site_repository : Repository.t
+  ; yocaml_repository : Repository.t
+  ; software_license : Link.t
+  ; content_license : Link.t
+  }
+
+let entity_name = "configuration"
+let neutral = Yocaml.Metadata.required entity_name
+
+let validate =
+  let open Yocaml.Data.Validation in
+  record (fun fields ->
+    let+ title = required fields "title" Field.not_blank
+    and+ subtitle = required fields "subtitle" Field.not_blank
+    and+ main_url = required fields "main_url" Url.validate
+    and+ site_repository = required fields "site_repository" Repository.validate
+    and+ yocaml_repository =
+      required fields "yocaml_repository" Repository.validate
+    and+ software_license = required fields "software_license" Link.validate
+    and+ content_license = required fields "content_license" Link.validate in
+    { title
+    ; subtitle
+    ; main_url
+    ; site_repository
+    ; yocaml_repository
+    ; software_license
+    ; content_license
+    })
+;;
+
+let normalize
+      { title
+      ; subtitle
+      ; main_url
+      ; site_repository
+      ; yocaml_repository
+      ; software_license
+      ; content_license
+      }
+  =
+  let open Yocaml.Data in
+  record
+    [ "title", string title
+    ; "subtitle", string subtitle
+    ; "main_url", Url.normalize main_url
+    ; "site_repository", Repository.normalize site_repository
+    ; "yocaml_repository", Repository.normalize yocaml_repository
+    ; "software_license", Link.normalize software_license
+    ; "content_license", Link.normalize content_license
+    ]
+;;
---
+++ lib/model/configuration.mli
@@ -0,0 +1,12 @@
+(** The global and common configuration for the project. *)
+
+type t
+
+(** {1 YOCaml related stuff} *)
+
+(** Since the goal is not to be injected as a full template, but as a
+    part of a generated artifact, we do not need to relay on
+    [DATA_INJECTABLE]. *)
+val normalize : t -> Yocaml.Data.t
+
+include Yocaml.Required.DATA_READABLE with type t := t
