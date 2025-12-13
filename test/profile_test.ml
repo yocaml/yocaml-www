1d20d04126f04b495e617b9ca0770cce4c877f1a
xvw <xavier.vdw@gmail.com>
2025-12-13 10:00:00 +0100

    retreive-from:xvw-patch-64:old/reapply

--- test/profile_test.ml
+++ test/profile_test.ml
@@ -29,7 +29,20 @@ let%expect_test "validating profile - 2 (from mailbox)" =
   [%expect
     {|
     [VALID] {"display_name": "Pierre Grim", "slug": "pierre-grim", "last_name":
-             null, "first_name": null, "avatar": null, "website": null, "email":
+             null, "first_name": null, "avatar":
+             {"target":
+              "https://www.gravatar.com/avatar/3d4f51b20864461509ebca757e75e887?",
+             "scheme": "https", "host": "www.gravatar.com", "path":
+              "/avatar/3d4f51b20864461509ebca757e75e887", "port": null, "query":
+              [{"fst": "", "snd": []}], "query_string": "", "has_port": false,
+             "has_query_string": true},
+            "website": null, "main_link":
+             {"name": "home", "url":
+              {"target": "/#", "scheme": "https", "host": "localhost", "path":
+               "/", "port": null, "query": [], "query_string": null, "has_port":
+               false, "has_query_string": false},
+             "description": "No associated link", "has_description": true},
+            "email":
              {"address": "grim@gmail.com", "local": "grim", "domain":
               "gmail.com", "domain_fragments": ["gmail", "com"], "address_md5":
               "3d4f51b20864461509ebca757e75e887"},
@@ -39,9 +52,9 @@ let%expect_test "validating profile - 2 (from mailbox)" =
              {"elements": [], "length": 0, "has_elements": false}, "more_emails":
              {"elements": [], "length": 0, "has_element": false}, "attributes":
              {"elements": [], "length": 0, "has_element": false},
-            "has_last_name": false, "has_first_name": false, "has_avatar":
-             false, "has_website": false, "has_email": true, "has_x_account":
-             false, "has_mastodon_account": false, "has_bsky_account": false}
+            "has_last_name": false, "has_first_name": false, "has_avatar": true,
+            "has_website": false, "has_email": true, "has_x_account": false,
+            "has_mastodon_account": false, "has_bsky_account": false}
     |}]
 ;;
 
@@ -54,9 +67,15 @@ let%expect_test "validating profile - 3 (from string)" =
   [%expect
     {|
     [VALID] {"display_name": "Pierre Grim", "slug": "pierre-grim", "last_name":
-             null, "first_name": null, "avatar": null, "website": null, "email":
-             null, "x_account": null, "mastodon_account": null, "bsky_account":
-             null, "more_accounts":
+             null, "first_name": null, "avatar": null, "website": null,
+            "main_link":
+             {"name": "home", "url":
+              {"target": "/#", "scheme": "https", "host": "localhost", "path":
+               "/", "port": null, "query": [], "query_string": null, "has_port":
+               false, "has_query_string": false},
+             "description": "No associated link", "has_description": true},
+            "email": null, "x_account": null, "mastodon_account": null,
+            "bsky_account": null, "more_accounts":
              {"elements": [], "length": 0, "has_elements": false}, "more_links":
              {"elements": [], "length": 0, "has_elements": false}, "more_emails":
              {"elements": [], "length": 0, "has_element": false}, "attributes":
@@ -85,7 +104,19 @@ let%expect_test "validating profile - 4 (from record with computed name)" =
   [%expect
     {|
     [VALID] {"display_name": "Pierre Grim", "slug": "pierre-grim", "last_name":
-             "Grim", "first_name": "Pierre", "avatar": null, "website": null,
+             "Grim", "first_name": "Pierre", "avatar":
+             {"target":
+              "https://www.gravatar.com/avatar/3d4f51b20864461509ebca757e75e887?",
+             "scheme": "https", "host": "www.gravatar.com", "path":
+              "/avatar/3d4f51b20864461509ebca757e75e887", "port": null, "query":
+              [{"fst": "", "snd": []}], "query_string": "", "has_port": false,
+             "has_query_string": true},
+            "website": null, "main_link":
+             {"name": "home", "url":
+              {"target": "/#", "scheme": "https", "host": "localhost", "path":
+               "/", "port": null, "query": [], "query_string": null, "has_port":
+               false, "has_query_string": false},
+             "description": "No associated link", "has_description": true},
             "email":
              {"address": "grim@gmail.com", "local": "grim", "domain":
               "gmail.com", "domain_fragments": ["gmail", "com"], "address_md5":
@@ -101,7 +132,7 @@ let%expect_test "validating profile - 4 (from record with computed name)" =
                 "address_md5": "3d4f51b20864461509ebca757e75e887"}}],
              "length": 1, "has_element": true},
             "attributes": {"elements": [], "length": 0, "has_element": false},
-            "has_last_name": true, "has_first_name": true, "has_avatar": false,
+            "has_last_name": true, "has_first_name": true, "has_avatar": true,
             "has_website": false, "has_email": true, "has_x_account": false,
             "has_mastodon_account": false, "has_bsky_account": false}
     |}]
@@ -127,7 +158,20 @@ let%expect_test "validating profile - 5 (from record)" =
   [%expect
     {|
     [VALID] {"display_name": "grm", "slug": "grm", "last_name": "Grim",
-            "first_name": "Pierre", "avatar": null, "website": null, "email":
+            "first_name": "Pierre", "avatar":
+             {"target":
+              "https://www.gravatar.com/avatar/3d4f51b20864461509ebca757e75e887?",
+             "scheme": "https", "host": "www.gravatar.com", "path":
+              "/avatar/3d4f51b20864461509ebca757e75e887", "port": null, "query":
+              [{"fst": "", "snd": []}], "query_string": "", "has_port": false,
+             "has_query_string": true},
+            "website": null, "main_link":
+             {"name": "x.com/grimfw", "url":
+              {"target": "https://x.com/grimfw", "scheme": "https", "host":
+               "x.com", "path": "/grimfw", "port": null, "query": [],
+              "query_string": null, "has_port": false, "has_query_string": false},
+             "description": null, "has_description": false},
+            "email":
              {"address": "grim@gmail.com", "local": "grim", "domain":
               "gmail.com", "domain_fragments": ["gmail", "com"], "address_md5":
               "3d4f51b20864461509ebca757e75e887"},
@@ -142,7 +186,7 @@ let%expect_test "validating profile - 5 (from record)" =
                 "address_md5": "3d4f51b20864461509ebca757e75e887"}}],
              "length": 1, "has_element": true},
             "attributes": {"elements": [], "length": 0, "has_element": false},
-            "has_last_name": true, "has_first_name": true, "has_avatar": false,
+            "has_last_name": true, "has_first_name": true, "has_avatar": true,
             "has_website": false, "has_email": true, "has_x_account": true,
             "has_mastodon_account": false, "has_bsky_account": false}
     |}]
@@ -168,7 +212,20 @@ let%expect_test "validating profile - 5 (from record)" =
   [%expect
     {|
     [VALID] {"display_name": "grm", "slug": "grm", "last_name": "Grim",
-            "first_name": "Pierre", "avatar": null, "website": null, "email":
+            "first_name": "Pierre", "avatar":
+             {"target":
+              "https://www.gravatar.com/avatar/3d4f51b20864461509ebca757e75e887?",
+             "scheme": "https", "host": "www.gravatar.com", "path":
+              "/avatar/3d4f51b20864461509ebca757e75e887", "port": null, "query":
+              [{"fst": "", "snd": []}], "query_string": "", "has_port": false,
+             "has_query_string": true},
+            "website": null, "main_link":
+             {"name": "x.com/grimfw", "url":
+              {"target": "https://x.com/grimfw", "scheme": "https", "host":
+               "x.com", "path": "/grimfw", "port": null, "query": [],
+              "query_string": null, "has_port": false, "has_query_string": false},
+             "description": null, "has_description": false},
+            "email":
              {"address": "grim@gmail.com", "local": "grim", "domain":
               "gmail.com", "domain_fragments": ["gmail", "com"], "address_md5":
               "3d4f51b20864461509ebca757e75e887"},
@@ -183,7 +240,7 @@ let%expect_test "validating profile - 5 (from record)" =
                 "address_md5": "3d4f51b20864461509ebca757e75e887"}}],
              "length": 1, "has_element": true},
             "attributes": {"elements": [], "length": 0, "has_element": false},
-            "has_last_name": true, "has_first_name": true, "has_avatar": false,
+            "has_last_name": true, "has_first_name": true, "has_avatar": true,
             "has_website": false, "has_email": true, "has_x_account": true,
             "has_mastodon_account": false, "has_bsky_account": false}
     |}]
